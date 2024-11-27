556018f468fc1f7327ec4e513b056d5eec68d706
Omer Majeed <o.n.k.majeed@student.tudelft.nl>
2024-11-27 21:45:26 +0100

    Class for the Collection entity

--- commons/src/main/java/commons/Collection.java
+++ commons/src/main/java/commons/Collection.java
@@ -2,6 +2,11 @@ package commons;
 
 
 import jakarta.persistence.*;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
 
 import java.util.List;
 
@@ -10,12 +15,58 @@ public class Collection {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    public long id;
+    private long id;
 
     private String name;
 
     @OneToMany
     private List<Note> notes;
 
+    @SuppressWarnings("unused")
+    private Collection() {
+        //this is for object mapping
+    }
 
+    /**
+     *
+     * @param list "a list of notes"
+     * @param name "name of the owner of the collection"
+     */
+    public Collection(String name, List<Note> list){
+        this.notes = list;
+        this.name = name;
+    }
+
+    /**
+     *
+     * @return list of notes
+     */
+    public List<Note> getNotes() {
+        return notes;
+    }
+
+    /**
+     * @param obj "random obj that is being compared"
+     * @return true if equal else false
+     */
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj);
+    }
+
+    /**
+     * @return hashcode
+     */
+    public int hashcode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    /**
+     * @return human-readable string format
+     */
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this,
+                ToStringStyle.MULTI_LINE_STYLE);
+    }
 }
+
