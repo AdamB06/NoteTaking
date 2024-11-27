78e8d0aef780d93abd80da55d4971386a8e8b5af
Omer Majeed <o.n.k.majeed@student.tudelft.nl>
2024-11-27 20:42:30 +0100

    these are the files for the note entity

--- commons/src/main/java/commons/Note.java
+++ commons/src/main/java/commons/Note.java
@@ -10,50 +10,75 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
+
 @Entity
 public class Note {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    public long id;
+    private long id;
 
-    public String title;
-    public String content;
+    private String title;
+    private String content;
 
     @SuppressWarnings("unused")
     private Note() {
         //this is for object mapping
     }
 
-
+    /**
+     * @param title   "title of the note"
+     * @param content "content of the note"
+     */
     public Note(String title, String content) {
         this.title = title;
         this.content = content;
     }
 
+    /**
+     * @return id
+     */
     public long getId() {
         return id;
+
     }
 
+    /**
+     * @return title
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * @return content
+     */
     public String getContent() {
         return content;
     }
 
+    /**
+     * @param obj "random obj that is being compared"
+     * @return true if equal else false
+     */
     @Override
     public boolean equals(Object obj) {
         return EqualsBuilder.reflectionEquals(this, obj);
     }
 
+    /**
+     * @return hashcode
+     */
     public int hashcode() {
         return HashCodeBuilder.reflectionHashCode(this);
     }
 
+    /**
+     * @return human-readable string format
+     */
     public String toString() {
-        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+        return ToStringBuilder.reflectionToString(this,
+                ToStringStyle.MULTI_LINE_STYLE);
     }
 
 
--- commons/src/test/java/commons/NoteTest.java
+++ commons/src/test/java/commons/NoteTest.java
@@ -9,7 +9,7 @@ public class NoteTest {
     private static final Note note = new Note("TestNote", "This is a Test");
 
     @Test
-    public void ConstructorTest(){
+    public void ConstructorTest() {
         assertEquals("TestNote", note.getTitle());
         assertNotEquals("Another test", note.getContent());
         assertNotNull(note);
@@ -17,7 +17,7 @@ public class NoteTest {
     }
 
     @Test
-    public void equalsHashCodeTest(){
+    public void equalsHashCodeTest() {
         Note note1 = new Note("TestNote", "This is a Test");
         Note note2 = new Note("TestNote", "This is another Test");
 
@@ -30,13 +30,12 @@ public class NoteTest {
     }
 
     @Test
-    public void ToStringTest(){
+    public void ToStringTest() {
 
-        var NoteTest = new Note("ToStringTest","This is the content").toString();
+        var NoteTest = new Note("ToStringTest", "This is the content").toString();
         assertTrue(NoteTest.contains("\n"));
         assertTrue(NoteTest.contains("Note"));
         assertTrue(NoteTest.contains(Note.class.getSimpleName()));
-
 
 
     }
