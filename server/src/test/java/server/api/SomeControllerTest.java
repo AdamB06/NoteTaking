73613cada7c7de3c8c5c89c5ffc563c52e019815
Georgi Yordanov <g.b.yordanov@student.tudelft.nl>
2024-11-29 14:02:27 +0100

    TwoAdditionalTestsForSomeControllerClass

--- server/src/test/java/server/api/SomeControllerTest.java
+++ server/src/test/java/server/api/SomeControllerTest.java
@@ -1,7 +1,9 @@
 package server.api;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.any;
+import commons.Collection;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -23,5 +25,35 @@ public class SomeControllerTest{
         var expected = "Hello world!";
         var actual = sut.index();
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testNameEndPoint_NewCollection(){
+        //Mock behavoir: in the case when the name is NOT present in the database
+        Mockito.when(db.existsByName(anyString())).thenReturn(false);
+
+        //Calling the method name
+        String response = sut.name("testName");
+
+        //Verifying that the response returns the expected outcome
+        assertEquals("Welcome, testName, to the application!", response);
+
+        //Verifying that the repository's "save" method was used and called once
+        Mockito.verify(db).save(any(Collection.class));
+    }
+
+    @Test
+    public void testNameEndPoint_ExistingCollection(){
+        //Mock behavoir: in the case when the name is present in the database
+        Mockito.when(db.existsByName(anyString())).thenReturn(true);
+
+        //Calling the method name
+        String response = sut.name("testName");
+
+        //Verifying that the response returns the expected outcome
+        assertEquals("Welcome, testName, to the application!", response);
+
+        //Veryfying that the repository's "save" method was not used
+        Mockito.verify(db, Mockito.never()).save(any(Collection.class));
     }
 }
