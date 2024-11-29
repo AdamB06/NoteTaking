2b2a669a330af9738d7ff8e8ae04e49eb04b52c2
Kelvin Li <K.Li-26@student.tudelft.nl>
2024-11-29 15:16:20 +0100

    inbetween save

--- client/src/main/java/client/scenes/TitleCtrl.java
+++ client/src/main/java/client/scenes/TitleCtrl.java
@@ -1,4 +1,28 @@
 package client.scenes;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class TitleCtrl {
+    private final TextField titleField;
+    private final Button editButton;
+
+    public TitleCtrl(TextField titleField, Button editButton){
+        this.titleField = titleField;
+        this.editButton = editButton;
+        initialize();
+    }
+
+    public void initialize(){
+        editButton.setOnAction(actionEvent -> {
+            titleField.setEditable(true); //Makes sure you can edit
+            titleField.requestFocus(); //Focuses on text field
+            titleField.selectAll(); //Selects everything in the text field
+        });
+    }
+
+    public void click(){
+
+    }
+
 }
