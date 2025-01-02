147354c4cbcf4a84dc926c1ad11098a8402e8390
Adam Bezard <a.s.bezard@student.tudelft.nl>
2025-01-02 20:43:40 +0100

    Fixed potential pipeline issue for file path, and added javadoc for checkstyle. Also fixed project dependencies that were preventing mvn clean install

--- client/src/main/java/client/ClientConfig.java
+++ client/src/main/java/client/ClientConfig.java
@@ -4,37 +4,61 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class ClientConfig {
-    private static final String CONFIG_FILE = "client/src/main/resources/config.json";
+    private static final String CONFIG_FILE = "config.json";
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
     private String preferredLanguage;
     private String serverUrl;
 
-    // Getters and setters
+    /**
+     * Get the preferred language of the client
+     * @return The preferred language of the client
+     */
     public String getPreferredLanguage() {
         return preferredLanguage;
     }
 
+    /**
+     * Set the preferred language of the client
+     * @param preferredLanguage The preferred language of the client
+     */
     public void setPreferredLanguage(String preferredLanguage) {
         this.preferredLanguage = preferredLanguage;
         saveConfig();
     }
 
+    /**
+     * Get the server URL
+     * @return The server URL
+     */
     public String getServerUrl() {
         return serverUrl;
     }
 
+    /**
+     * Set the server URL
+     * @param serverUrl The server URL
+     */
     public void setServerUrl(String serverUrl) {
         this.serverUrl = serverUrl;
         saveConfig();
     }
 
-    // Load configuration from file
+    /**
+     * Load the configuration file
+     * @return The configuration file contents
+     */
     public static ClientConfig loadConfig() {
-        try {
-            return objectMapper.readValue(new File(CONFIG_FILE), ClientConfig.class);
+        try (InputStream inputStream = ClientConfig.class.getClassLoader()
+                .getResourceAsStream(CONFIG_FILE)) {
+            if (inputStream != null) {
+                return objectMapper.readValue(inputStream, ClientConfig.class);
+            } else {
+                throw new IOException("Configuration file not found");
+            }
         } catch (IOException e) {
             ClientConfig defaultConfig = new ClientConfig();
             defaultConfig.setPreferredLanguage("en");
@@ -44,10 +68,13 @@ public class ClientConfig {
         }
     }
 
-    // Save configuration to file
+    /**
+     * Save/Update the configuration file
+     */
     private void saveConfig() {
         try {
-            objectMapper.writeValue(new File(CONFIG_FILE), this);
+            objectMapper.writeValue(new File(getClass().getClassLoader()
+                    .getResource(CONFIG_FILE).getFile()), this);
         } catch (IOException e) {
             e.printStackTrace();
         }
