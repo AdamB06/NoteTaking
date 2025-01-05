3edacd74fdc1a55e9b09a4d2cedaff58b87039a4
Omer Majeed <o.n.k.majeed@student.tudelft.nl>
2025-01-05 13:07:46 +0100

    added a query to the noterepository named findnotesbytagname

--- server/src/main/java/server/database/NoteRepository.java
+++ server/src/main/java/server/database/NoteRepository.java
@@ -3,6 +3,18 @@ package server.database;
 import commons.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
 public interface NoteRepository extends JpaRepository<Note, Long> {
+
+    @Query("SELECT n FROM Note n JOIN n.tags t WHERE t.name = :tagName")
+    List<Note> findNotesByTagName(@Param("tagName") String tagName);
+
 
 }
