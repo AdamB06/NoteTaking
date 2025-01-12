af951603a33ac0a8a1a74671f275320ad3471d6e
Georgi Yordanov <g.b.yordanov@student.tudelft.nl>
2025-01-12 23:05:05 +0100

    showEditMethod - PrimaryCtrl

--- client/src/main/java/client/scenes/PrimaryCtrl.java
+++ client/src/main/java/client/scenes/PrimaryCtrl.java
@@ -8,15 +8,19 @@ import javafx.util.Pair;
 public class PrimaryCtrl {
     private Stage primaryStage;
     private Scene homeScene;
+    private Scene editScene;
+    private EditCollectionCtrl editCtrl;
 
     /**
      * Initialize the UI controller
      * @param primaryStage Variable for the stage
      * @param home Pair of UI and Controller
      */
-    public void init(Stage primaryStage, Pair<HomePageCtrl, Parent> home) {
+    public void init(Stage primaryStage, Pair<HomePageCtrl, Parent> home, Pair<EditCollectionCtrl, Parent> edit) {
         this.primaryStage = primaryStage;
         this.homeScene = new Scene(home.getValue());
+        this.editScene = new Scene(edit.getValue());
+        this.editCtrl = edit.getKey();
         this.primaryStage.setOnCloseRequest(event -> {
             home.getKey().forceSaveBeforeClose();
         });
@@ -30,5 +34,16 @@ public class PrimaryCtrl {
     public void showHome() {
         primaryStage.setTitle("NetNote");
         primaryStage.setScene(homeScene);
+    }
+
+    /**
+     * Displays the Edit Collection UI
+     * If the key "ESCAPE" is pressed then we exit the scene
+     * and should go back to the main one
+     */
+    public void showEdit(){
+        primaryStage.setTitle("Edit Collection");
+        primaryStage.setScene(editScene);
+        editScene.setOnKeyPressed(event -> editCtrl.keyPressed(event));
     }
 }
