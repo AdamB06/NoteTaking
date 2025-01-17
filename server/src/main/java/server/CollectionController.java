d0fa77dd0c4120f1fb42c59f27add51414ea5501
Aidan Voskamp <a.voskamp@student.tudelft.nl>
2025-01-17 12:00:14 +0100

    Added addNote and deleteNote to accept requests to the server

--- server/src/main/java/server/CollectionController.java
+++ server/src/main/java/server/CollectionController.java
@@ -45,6 +45,17 @@ public class CollectionController {
     }
 
     /**
+     * Endpoint to delete a collection by id
+     * @param id id of the collection to be deleted
+     * @return A ResponseEntity containing a message if the method was successfully executed
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCollection(@PathVariable long id) {
+        collectionService.deleteCollectionById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    /**
      * Endpoint to edit the name of a collection
      * @param name The new name
      * @param id The id of the collection to be edited
@@ -75,17 +86,6 @@ public class CollectionController {
     }
 
     /**
-     * Endpoint to delete a collection by id
-     * @param id id of the collection to be deleted
-     * @return A ResponseEntity containing a message if the method was successfully executed
-     */
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteCollection(@PathVariable long id) {
-        collectionService.deleteCollectionById(id);
-        return ResponseEntity.ok().build();
-    }
-
-    /**
      * Endpoint to getting a collection by id
      * @param id id of the collection to get
      * @return A ResponseEntity containing the collection
@@ -94,5 +94,29 @@ public class CollectionController {
     public ResponseEntity<Collection> getCollectionById(long id) {
         Collection collection = collectionService.getCollectionById(id);
         return ResponseEntity.ok(collection);
+    }
+
+    /**
+     * Endpoint to add a note to a collection
+     * @param id The id of the collection
+     * @param note The Note to be added
+     * @return A ResponseEntity containing a message if the method was successfully executed
+     */
+    @PutMapping("/NoteAdd/{id}")
+    public ResponseEntity<Void> addNote(@PathVariable long id, @RequestBody Note note) {
+        collectionService.addNoteToCollection(id, note);
+        return ResponseEntity.ok().build();
+    }
+
+    /**
+     * Endpoint to delete a note from a collection
+     * @param collectionId The id of the collection
+     * @param noteId The id of the Note to be deleted
+     * @return A ResponseEntity containing a message if the method was successfully executed
+     */
+    @DeleteMapping("/NoteDelete/{collectionId}/{noteId}")
+    public ResponseEntity<Void> deleteNote(@PathVariable long collectionId, @PathVariable long noteId) {
+        collectionService.deleteNoteFromCollection(collectionId, noteId);
+        return ResponseEntity.ok().build();
     }
 }
