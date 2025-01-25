c337e7e46b50076b75c810a03acf9dfc6e61e6b7
Kevin Moonen <K.Moonen@student.tudelft.nl>
2025-01-25 20:02:46 +0100

    Additional tests for the LanguageController class

--- client/src/test/java/LanguageControllerTest.java
+++ client/src/test/java/LanguageControllerTest.java
@@ -9,12 +9,12 @@ public class LanguageControllerTest {
     private LanguageController lc;
 
     @BeforeEach
-    public void init(){
+    public void init() {
         lc = new LanguageController();
     }
 
     @Test
-    public void testLanguageLoad(){
+    public void testLoadEnglish() {
         String lang = "en";
         lc.loadLanguage(lang);
 
@@ -25,7 +25,29 @@ public class LanguageControllerTest {
     }
 
     @Test
-    public void testLanguageSwitch(){
+    public void testLoadDefault(){
+        String lang = "adfsdf";
+        lc.loadLanguage(lang);
+
+        String actual = lc.getEditText();
+        String expected = "Edit";
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testLoadGerman() {
+        String lang = "de";
+        lc.loadLanguage(lang);
+
+        String actual = lc.getEditText();
+        String expected = "Bearbeiten";
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testLanguageSwitch() {
         String lang = "en";
         lc.loadLanguage(lang);
 
@@ -54,14 +76,43 @@ public class LanguageControllerTest {
     }
 
     @Test
-    public void testPersistence(){
-        String lang="en";
+    public void testLanguageSwitchDifferent() {
+        String lang = "de";
+        lc.loadLanguage(lang);
+
+        String actual = lc.getEditText();
+        String expected = "Bearbeiten";
+
+        assertEquals(expected, actual);
+
+        lang = "es";
+        lc.loadLanguage(lang);
+
+        actual = lc.getEditText();
+        expected = "Editar";
+
+        assertEquals(expected, actual);
+
+        lang = "nl";
+
+
+        lc.loadLanguage(lang);
+
+        actual = lc.getEditText();
+        expected = "Aanpassen";
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testPersistence() {
+        String lang = "en";
         lc.loadLanguage(lang);
 
         LanguageController lc2 = new LanguageController();
         lc2.loadLanguage(lang);
 
-        String expected=lc.getEditText();
+        String expected = lc.getEditText();
         String actual = lc2.getEditText();
 
         assertEquals(actual, expected);
